5d4dd0c82d253a79e20ed9bafa07ee54bb3ea0c9
John Andrews <johnnydrews@gmail.com>
2013-12-11 20:17:20 -0500

    first commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by John Andrews on 12/11/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by John Andrews on 12/11/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
