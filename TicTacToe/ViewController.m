6d52a309f6358639d8f4c098f7e6c68c49bf46c2
John Andrews <johnnydrews@gmail.com>
2013-12-13 11:55:22 -0600

    Computer does not use unavailable game board cells

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -11,9 +11,7 @@
 @interface ViewController ()
 {
     NSArray *gameBoardCoordinatesArray;
-    NSMutableArray *unavailableGameBoardPositionsArray;    
-    NSDictionary *ticTacToeGameBoardPositionsDictionary;
-    
+    NSMutableArray *unavailableGameBoardPositionsArray;        
     NSTimer *displayComputersMove;
 }
 
@@ -26,21 +24,8 @@
 {    
     [super viewDidLoad];
     self.coordinateInputTextField.delegate = self;
-    
-    [self beingGame];
-    
     gameBoardCoordinatesArray = @[@"0,0", @"1,0", @"2,0", @"0,1", @"1,1", @"2,1", @"0,2", @"1,2", @"2,2"];
-    
-    ticTacToeGameBoardPositionsDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-                                             topRightCornerLabel, @"0",
-                                             topMiddleLabel, @"1", 
-                                             topRightCornerLabel, @"2", 
-                                             middleLeftLabel, @"3", 
-                                             middleMiddleLabel, @"4", 
-                                             middleRightLabel, @"5", 
-                                             bottomLeftCornerLabel, @"6", 
-                                             bottomMiddleLabel, @"7", 
-                                             bottomRightCornerLabel, @"8", nil];
+    [self beingGame];
 }
 
 - (void)beingGame
@@ -53,100 +38,131 @@
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
+    NSLog(@"Player's move is %@", textField.text);
     [textField resignFirstResponder];
     [self showAnyPlayersMove:textField.text markerToBeUsed:@"X"];
     textField.text = nil;
+    displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
     return NO;
 }
 
 
 - (void)showAnyPlayersMove:(NSString *)usersMove markerToBeUsed:(NSString *)marker
 {
-    NSLog(@"User's move was %@", usersMove);
     if ([usersMove isEqualToString:@"0,0"] || [usersMove isEqualToString:@"0, 0"]) {
         topLeftCornerLabel.font = [topLeftCornerLabel.font fontWithSize:45.0f];
         topLeftCornerLabel.textColor = [UIColor blackColor];
         topLeftCornerLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)topLeftCornerLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"1,0"] || [usersMove isEqualToString:@"1, 0"]) {
         topMiddleLabel.font = [topMiddleLabel.font fontWithSize:45.0f];
         topMiddleLabel.textColor = [UIColor blackColor];
         topMiddleLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)topMiddleLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"2,0"] || [usersMove isEqualToString:@"2, 0"]) {
         topRightCornerLabel.font = [topRightCornerLabel.font fontWithSize:45.0f];
         topRightCornerLabel.textColor = [UIColor blackColor];
         topRightCornerLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)topRightCornerLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"0,1"] || [usersMove isEqualToString:@"0, 1"]) {
+        [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%d",middleLeftLabel.tag]];
         middleLeftLabel.font = [middleLeftLabel.font fontWithSize:45.0f];
         middleLeftLabel.textColor = [UIColor blackColor];
         middleLeftLabel.text = [NSString stringWithFormat:@"%@", marker];
-        [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)middleLeftLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"1,1"] || [usersMove isEqualToString:@"1, 1"]) {
         middleMiddleLabel.font = [middleMiddleLabel.font fontWithSize:45.0f];
         middleMiddleLabel.textColor = [UIColor blackColor];
         middleMiddleLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)middleMiddleLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"2,1"] || [usersMove isEqualToString:@"2, 1"]) {
         middleRightLabel.font = [middleRightLabel.font fontWithSize:45.0f];
         middleRightLabel.textColor = [UIColor blackColor];
         middleRightLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)middleRightLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"0,2"] || [usersMove isEqualToString:@"0, 2"]) {
         bottomLeftCornerLabel.font = [bottomLeftCornerLabel.font fontWithSize:45.0f];
         bottomLeftCornerLabel.textColor = [UIColor blackColor];
         bottomLeftCornerLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)bottomLeftCornerLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"1,2"] || [usersMove isEqualToString:@"1, 2"]) {
         bottomMiddleLabel.font = [bottomMiddleLabel.font fontWithSize:45.0f];
         bottomMiddleLabel.textColor = [UIColor blackColor];
         bottomMiddleLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)bottomMiddleLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else if ([usersMove isEqualToString:@"2,2"] || [usersMove isEqualToString:@"2, 2"]) {
         bottomRightCornerLabel.font = [bottomRightCornerLabel.font fontWithSize:45.0f];
         bottomRightCornerLabel.textColor = [UIColor blackColor];
         bottomRightCornerLabel.text = [NSString stringWithFormat:@"%@", marker];
         [unavailableGameBoardPositionsArray addObject:[NSString stringWithFormat:@"%ld",(long)bottomRightCornerLabel.tag]];
-        displayComputersMove = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(figureOutComputersMove) userInfo:nil repeats:NO];
         
     } else {
         UIAlertView *playerMadeWrongEntryAlert = [[UIAlertView alloc] initWithTitle:@"Incorrect Entry" message:@"Please Enter Coordinates in a #,# format" delegate:self cancelButtonTitle:@"Try Again" otherButtonTitles:nil];
         [playerMadeWrongEntryAlert show];
     }
+    //[self catsGame];
+    //[self eightWaysToWinTicTacToe];
     NSLog(@"taken game board positions are %@", unavailableGameBoardPositionsArray);
-    
-    //call method to check if person has tic tac toe*****
-    
-    
 }
+
 
 - (void)figureOutComputersMove
 {   
     //computer has to figure out if there is already an X in tic tac to box
-    int randomComputerMove = arc4random() % 8;
-    int cycleThroughTagNumbers = 0;
-    while (randomComputerMove != [[unavailableGameBoardPositionsArray objectAtIndex:cycleThroughTagNumbers] integerValue]) {
-        NSLog(@"Computers Move is %@", [gameBoardCoordinatesArray objectAtIndex:randomComputerMove]);
-        [self showAnyPlayersMove:[gameBoardCoordinatesArray objectAtIndex:randomComputerMove] markerToBeUsed:@"O"];
-        cycleThroughTagNumbers++;
+    /*
+    NSArray* yourArray = [NSArray arrayWithObjects: @"Str1", @"Str2", @"Str3", nil];
+    if ( [yourArray containsObject: yourStringToFind] ) {
+        // do found
+    } else {
+        // do not found
+    }*/
+    BOOL moveWorks = NO;
+    while ((moveWorks = YES)) {
+        int randomComputerMove = arc4random() % 8;
+        if (![unavailableGameBoardPositionsArray containsObject:[NSString stringWithFormat:@"%d", randomComputerMove]]) { 
+            NSLog(@"Random selection (%d) is NOT in the unavailableGameBoardPositionsArray", randomComputerMove);
+            NSLog(@"Computers Move is %@", [gameBoardCoordinatesArray objectAtIndex:randomComputerMove]);
+            [self showAnyPlayersMove:[gameBoardCoordinatesArray objectAtIndex:randomComputerMove] markerToBeUsed:@"O"];
+            moveWorks = NO;
+            break;
+        } else {
+            NSLog(@"random selection (%d) is in the array", randomComputerMove);
+            randomComputerMove++;
+        }
     }
+    
+    /*
+    
+    for (NSString *usedGameBoardPositions in unavailableGameBoardPositionsArray){
+        int randomComputerMove = arc4random() % 8;
+        NSString *potentialComputerMove = [NSString stringWithFormat:@"%d", randomComputerMove];
+        NSLog(@"usedGameBoardPositions = %@", usedGameBoardPositions);
+        
+        if (randomComputerMove == [usedGameBoardPositions integerValue]) {
+            NSLog(@"conmupters move was going to be %d", randomComputerMove);
+            randomComputerMove++;
+            continue;
+        } else {
+            NSLog(@"Computers Move is %@", [gameBoardCoordinatesArray objectAtIndex:randomComputerMove]);
+            [self showAnyPlayersMove:[gameBoardCoordinatesArray objectAtIndex:randomComputerMove] markerToBeUsed:@"O"];
+            break;
+        }*/
+//        if (![potentialComputerMove isEqualToString:usedGameBoardPositions]) {
+//            NSLog(@"Computers Move is %@", [gameBoardCoordinatesArray objectAtIndex:[potentialComputerMove integerValue]]);
+//            [self showAnyPlayersMove:[gameBoardCoordinatesArray objectAtIndex:[potentialComputerMove integerValue]] markerToBeUsed:@"O"];
+//            break;
+//        } else {
+//            randomComputerMove++;
+//        }
+//    }
+    
 }
 
 - (void)eightWaysToWinTicTacToe
@@ -187,35 +203,16 @@
     }
 }
 
-/*
-- (BOOL)checkIfTicTacToeWinner
+- (void)catsGame
 {
-    if (<#condition#>) {
-        UIAlertView *playerWonGame = [[UIAlertView alloc] initWithTitle:@"Winner" message:@"Player [X|O] won!" delegate:self cancelButtonTitle:@"Play Again" otherButtonTitles:nil];
-        [playerWonGame show];
-        return YES;
-    } 
-    if (    ){
+    if (unavailableGameBoardPositionsArray.count - 1 == 8) {
         UIAlertView *catsGame = [[UIAlertView alloc] initWithTitle:@"Cat's Game" message:@"Stalemate!" delegate:self cancelButtonTitle:@"Play Again" otherButtonTitles:nil];
         [catsGame show];
-        return NO;
-    } else {
-        return NO;
     }
-}
-*/
-
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-    if (buttonIndex == 0) {
-        [self restartGame];
-    } 
-    
+    [self beingGame];
 }
 
-- (void)restartGame
-{
-    
-}
+
+
 
 @end
